dcde748c717569472fd1813c2b5fc7512bbdb2b5
I3ubble <rongcan@ualberta.ca>
2024-02-28 12:11:58 -0700

    Worked on Attendee Customize profile page

--- EventApp/app/src/main/java/com/example/eventapp/attendee/CustomizeProfile.java
+++ EventApp/app/src/main/java/com/example/eventapp/attendee/CustomizeProfile.java
@@ -1,14 +1,29 @@
 package com.example.eventapp.attendee;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventapp.R;
+
+import java.util.Objects;
+
 public class CustomizeProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.customize_profile);
+        setContentView(R.layout.customize_attendee_profile);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Customize Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Navigate back to AttendeeAccount
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
