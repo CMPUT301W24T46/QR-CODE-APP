d7e1d5268b0752024ca6509a753bc5632cd6afe5
I3ubble <rongcan@ualberta.ca>
2024-02-27 20:20:01 -0700

    Added Customize Profile page

--- EventApp/app/src/main/java/com/example/eventapp/attendee/AttendeeAccount.java
+++ EventApp/app/src/main/java/com/example/eventapp/attendee/AttendeeAccount.java
@@ -1,5 +1,6 @@
 package com.example.eventapp.attendee;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.eventapp.R;
 
@@ -61,6 +63,18 @@ public class AttendeeAccount extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attendee_account, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_attendee_account, container, false);
+
+        Button btnCustomizeProfile = rootView.findViewById(R.id.btnCustomizeProfile);
+
+        btnCustomizeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CustomizeProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
---
+++ EventApp/app/src/main/java/com/example/eventapp/attendee/CustomizeProfile.java
@@ -0,0 +1,14 @@
+package com.example.eventapp.attendee;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventapp.R;
+public class CustomizeProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customize_profile);
+    }
+}
