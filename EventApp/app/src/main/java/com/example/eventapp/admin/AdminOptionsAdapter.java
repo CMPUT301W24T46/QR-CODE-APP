990aa5a7cbca868471cb7a2f515895acbf28f550
JerryYu-2004 <71236947+JerryYu-2004@users.noreply.github.com>
2024-03-06 16:49:14 -0700

    Update AdminOptionsAdapter.java

--- EventApp/app/src/main/java/com/example/eventapp/admin/AdminOptionsAdapter.java
+++ EventApp/app/src/main/java/com/example/eventapp/admin/AdminOptionsAdapter.java
@@ -19,7 +19,6 @@ public class AdminOptionsAdapter extends ArrayAdapter<String> {
     private String[] options;
 
     public AdminOptionsAdapter(Context context, String[] options) {
-        // Since you're customizing the view, pass in 0 for the resource ID and your options array.
         super(context, 0, options);
         this.context = context;
         this.options = options;
@@ -28,20 +27,15 @@ public class AdminOptionsAdapter extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.cardview_admin_option, parent, false);
         }
 
-        // Get the data item for this position
         String option = getItem(position);
 
-        // Lookup view for data population
         TextView textView = convertView.findViewById(R.id.adminOption);
-        // Populate the data into the template view using the data object
         textView.setText(option);
 
-        // Return the completed view to render on screen
         return convertView;
     }
 }
