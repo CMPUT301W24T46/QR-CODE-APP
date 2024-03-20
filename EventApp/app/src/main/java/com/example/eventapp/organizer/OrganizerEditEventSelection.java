0331886c8b6e5dc560f9d8bc93393aacee008af2
Jerry Yu <JerryYu8282@Gmail.com>
2024-03-19 20:58:39 -0600

    implemented get user location

--- EventApp/app/src/main/java/com/example/eventapp/organizer/OrganizerEditEventSelection.java
+++ EventApp/app/src/main/java/com/example/eventapp/organizer/OrganizerEditEventSelection.java
@@ -78,10 +78,15 @@ public class OrganizerEditEventSelection extends Fragment {
 //            Navigation.findNavController(v).navigate(R.id.);
 //        });
 //
-//        Button btnLocationCheckIn = view.findViewById(R.id.btn_locationCheckIn);
-//        btnLocationCheckIn.setOnClickListener(v -> {
-//            // Handle location check-in button click
-//        });
+
+        Button btnLocationCheckIn = view.findViewById(R.id.btn_locationCheckIn);
+        btnLocationCheckIn.setOnClickListener(v -> {
+            Bundle eventBundle = new Bundle();
+            eventBundle.putString("eventId", eventId1);
+            navController.navigate(R.id.action_organizer_edit_event_selection_to_organizer_event_map, eventBundle);
+            Log.d("OrganizerEditEventSelection", "EventId passed: " + eventId1);
+        });
+
         navController = Navigation.findNavController(view);
 
 
