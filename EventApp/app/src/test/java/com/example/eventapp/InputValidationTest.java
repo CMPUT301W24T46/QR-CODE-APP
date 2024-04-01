88c3dbb04379177ae2e1843c36cefbb977edc323
andrew786e <74216051+andrew786e@users.noreply.github.com>
2024-04-01 14:12:13 -0600

    Automated Picture Done and Sign Up Complete

--- EventApp/app/src/test/java/com/example/eventapp/InputValidationTest.java
+++ EventApp/app/src/test/java/com/example/eventapp/InputValidationTest.java
@@ -1,5 +1,6 @@
 package com.example.eventapp;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -33,5 +34,23 @@ public class InputValidationTest {
     public void testContainsLetters() {
         assertFalse(checkCustomizeProfileData.containsLetters("12345abc"));
         assertTrue(checkCustomizeProfileData.containsLetters("123456"));
+    }
+
+    @Test
+    public void testProfilePicDetermination(){
+        // Case when first character is between 'a' and 'h'
+        assertEquals("First", CheckCustomizeProfileData.determineProfilePic("apple"));
+        assertEquals("First", CheckCustomizeProfileData.determineProfilePic("Apple"));
+
+        // Case when first character is 'i' or 'j'
+        assertEquals("Second", CheckCustomizeProfileData.determineProfilePic("ice"));
+        assertEquals("Second", CheckCustomizeProfileData.determineProfilePic("jack"));
+
+        // Case when first character is outside 'a' to 'j'
+        assertEquals("Third", CheckCustomizeProfileData.determineProfilePic("kite"));
+        assertEquals("Third", CheckCustomizeProfileData.determineProfilePic("Zebra"));
+
+        // Case when username is an empty string
+        assertEquals("", CheckCustomizeProfileData.determineProfilePic(""));
     }
 }
